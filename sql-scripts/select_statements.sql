6f942094ab53cab337be9cd293569a30e67b5402
Rashmi Sharma <rashmisharmakarki@gmail.com>
2021-11-22 16:59:40 -0700

    practice select statements

---
+++ sql-scripts/select_statements.sql
@@ -0,0 +1,39 @@
+SELECT 'hello world' AS method;
+
+
+SELECT 'good day!'; 
+
+
+SELECT 'okay';
+
+SELECT *
+FROM HumanResources.Employee;
+
+
+SELECT top 20 *
+FROM HumanResources.Department;
+
+SELECT top 5 DepartmentID, Name
+FROM HumanResources.Department;
+
+SELECT top 5 DepartmentID AS ID, Name AS 'Department Name'
+FROM HumanResources.Department;
+
+
+SELECT top 10 DepartmentID AS ID, Name AS 'Department Name'
+FROM HumanResources.Department
+ORDER BY ID DESC;
+
+
+SELECT top 10 DepartmentID AS ID, Name AS 'Department Name'
+FROM HumanResources.Department
+ORDER BY ID;
+
+
+
+SELECT DepartmentID AS ID, Name AS 'Department Name'
+FROM HumanResources.Department AS hr_department
+ORDER BY ID
+offset 3 rows
+fetch next 3 rows only;
+
