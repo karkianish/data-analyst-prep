82038f6a3a28b32da543d64cfc485498fad0a234
Anish Karki <karkianish@hotmail.com>
2021-11-24 09:06:50 -0700

    groupby examples

--- sql-scripts/joins.sql
+++ sql-scripts/joins.sql
@@ -1,15 +1,16 @@
 
 -- join
-create table #people (id int, first_name varchar(100), last_name varchar(100));
+drop table #people
+create table #people (id int, first_name varchar(100), last_name varchar(100), salary int);
 
 create table #order (id int, people_id int, total decimal, create_date datetime);
 
-insert into #people(id, first_name, last_name)
+insert into #people(id, first_name, last_name, salary)
 values 
-(1, 'anish', 'karki'),
-(2, 'rashmi', 'sharma'),
-(3, 'anil', 'panta'),
-(4, 'binita', 'chettri')
+(1, 'anish', 'karki', 1000),
+(2, 'rashmi', 'karki', 2000),
+(3, 'anil', 'panta', 3000),
+(4, 'binita', 'panta', 4000)
 
 insert into #order(id, people_id, total, create_date)
 values
@@ -79,7 +80,25 @@ select * from cte_people_and_order
 union 
 select * from cte_people_and_order;
 
+select @@VERSION
+--- 11/24/2021
+-- group by, having, where and or, distinct, count, avg, max, min
+
+select sum(max_salary) as total_max_salary 
+from (
+	select last_name, max(salary) as max_salary
+	from #people
+	--where first_name != 'rashmi'
+	group by last_name) as abc
+
+select * 
+from (select *, 
+		RANK() over (partition by last_name order by salary desc) as row_num		
+		from #people) as x 
+where x.row_num = 1
 
 
 
+--having max(first_name) = 'rashmi'
 
+select * from #order
